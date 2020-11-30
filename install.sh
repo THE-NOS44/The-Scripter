fbbf4a41f5e5b57618356dba93e24d0910606c93
THE-NOS44 <44903050+THE-NOS44@users.noreply.github.com>
2020-11-30 15:27:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 
 apt-get install figlet
 
-echo "Installing Stabilize Shell Dependencies"
 
 apt-get install xdotool
 sudo apt-get install xautomation
@@ -10,8 +9,6 @@ sudo apt-get install xautomation
 echo "Installing SSH Cracker Dependencies"
 
 apt-get install john -y
-
-echo "Installing Hash Cracker Dependencies"
 
 pip3 install futures
 pip3 install argparse
