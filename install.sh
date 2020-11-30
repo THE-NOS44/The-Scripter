e1aad931afcf43813c9bee5832fd9cdd8e24af19
warlord-vj <71816380+warlord-vj@users.noreply.github.com>
2020-11-30 16:50:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,12 +21,12 @@ sudo apt-get install docker.io
 
 
 chmod +x $PWD/the_scripter.sh
-chmod +x $PWD/scripts/function.sh
-chmod +x $PWD/scripts/myrecon
-chmod +x $PWD/scripts/Password_Genrator.sh
-chmod +x $PWD/scripts/payloadgen.sh
-chmod +x $PWD/scripts/ssh_cracker
-chmod +x $PWD/scripts/webserver.sh
+chmod +x $PWD/script/function.sh
+chmod +x $PWD/script/myrecon
+chmod +x $PWD/script/Password_Genrator.sh
+chmod +x $PWD/script/payloadgen.sh
+chmod +x $PWD/script/ssh_cracker
+chmod +x $PWD/script/webserver.sh
 
 
 
