0d019396e025d350e07667e5eaee6a5745f31a06
THE-NOS44 <44903050+THE-NOS44@users.noreply.github.com>
2020-11-30 15:26:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -24,17 +24,11 @@ sudo apt-get install docker.io
 
 
 chmod +x $PWD/the_scripter.sh
-chmod +x $PWD/scripts/juice.sh
-chmod +x $PWD/scripts/add_cronjob.sh
-chmod +x $PWD/scripts/add_ssh_key.sh
 chmod +x $PWD/scripts/function.sh
-chmod +x $PWD/scripts/Hash_Cracker.py
 chmod +x $PWD/scripts/myrecon
 chmod +x $PWD/scripts/Password_Genrator.sh
 chmod +x $PWD/scripts/payloadgen.sh
-chmod +x $PWD/scripts/shell_stabalize.sh
 chmod +x $PWD/scripts/ssh_cracker
-chmod +x $PWD/scripts/stabilize_zsh_shell.sh
 chmod +x $PWD/scripts/webserver.sh
 
 
